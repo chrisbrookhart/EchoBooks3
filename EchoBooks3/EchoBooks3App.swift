863211f43c6a50f212de1ec1eb43fe6094337446
chrisbrookhart <55410652+chrisbrookhart@users.noreply.github.com>
2025-02-05 19:44:25 -0700

    Initial Commit

---
+++ EchoBooks3/EchoBooks3App.swift
@@ -0,0 +1,32 @@
+//
+//  EchoBooks3App.swift
+//  EchoBooks3
+//
+//  Created by Chris Brookhart on 2/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct EchoBooks3App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ EchoBooks3Tests/EchoBooks3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  EchoBooks3Tests.swift
+//  EchoBooks3Tests
+//
+//  Created by Chris Brookhart on 2/5/25.
+//
+
+import Testing
+@testable import EchoBooks3
+
+struct EchoBooks3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

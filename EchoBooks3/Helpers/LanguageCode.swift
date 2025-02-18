e9f9cc8c64e2cc06c55cf6e94c7a601c90b8c099
chrisbrookhart <55410652+chrisbrookhart@users.noreply.github.com>
2025-02-17 21:52:24 -0700

    Deleted old commented out code

--- EchoBooks3/Helpers/LanguageCode.swift
+++ EchoBooks3/Helpers/LanguageCode.swift
@@ -30,37 +30,3 @@ enum LanguageCode: String, Codable, CaseIterable, Hashable, Identifiable {
 }
 
 
-
-
-////
-////  LanguageCode.swift
-////  YourAppName
-////
-////  Created by [Your Name] on [Date].
-////
-// 
-//import Foundation
-//
-//enum LanguageCode: String, Codable, CaseIterable, Hashable, Identifiable {
-//    case enUS = "en-US"
-//    case esES = "es-ES"
-//    case frFR = "fr-FR"
-//    case deDE = "de-DE"
-//    case jaJP = "ja-JP"
-//    case zhCN = "zh-CN"
-//    case tlPH = "tl-PH"
-//    
-//    var id: String { self.rawValue }
-//    
-//    var name: String {
-//        switch self {
-//        case .enUS: return "English (US)"
-//        case .esES: return "Spanish (ES)"
-//        case .frFR: return "French (FR)"
-//        case .deDE: return "German (DE)"
-//        case .jaJP: return "Japanese (JP)"
-//        case .zhCN: return "Mandarin (CN)"
-//        case .tlPH: return "Tagalog (PH)"
-//        }
-//    }
-//}
